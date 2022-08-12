79f2f963bdc0116486fc6631f2dad7390d579f91
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2022-08-12 09:43:35 +0530

    Demo adding of middleware

--- store/store.js
+++ store/store.js
@@ -1,12 +1,16 @@
 const { configureStore } = require("@reduxjs/toolkit");
+const { logger } = require("redux-logger");
 
-const cakeReducer = require('./slices/cakeSlice');
-const iceCreamReducer = require('./slices/iceCreamSlice');
+const cakeReducer = require("./slices/cakeSlice");
+const iceCreamReducer = require("./slices/iceCreamSlice");
 
 const store = configureStore({
-    reducer: {
-        cake: cakeReducer, iceCream: iceCreamReducer
-    }
-})
+  reducer: {
+    cake: cakeReducer,
+    iceCream: iceCreamReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat([logger, logger]),
+});
 
 module.exports = store;
