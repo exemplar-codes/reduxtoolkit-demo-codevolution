8fd5cf7adc0d1fb1d6e0a8b8410f56c780aca812
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2022-08-12 10:54:56 +0530

    Demo use of createAsyncThunk

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const store = require("./store/store");
-const { cakeActions } = require("./store/slices/cakeSlice");
+const { fetchUsers } = require("./store/slices/userSlice");
 
 store.subscribe(() => console.log(store.getState()));
 
 console.log(store.getState());
-store.dispatch(cakeActions.orderCake());
-store.dispatch(cakeActions.orderCake());
+// store.dispatch(cakeActions.orderCake());
+// store.dispatch(cakeActions.orderCake());
+store.dispatch(fetchUsers());
--- store/slices/userSlice.js
+++ store/slices/userSlice.js
@@ -1,30 +1,11 @@
-const { createSlice } = require("@reduxjs/toolkit");
+const { createSlice, createAsyncThunk } = require("@reduxjs/toolkit");
+const axios = require("axios").default;
 
 const initialState = {
   loading: false,
   users: [],
   error: "",
 };
-
-const asyncSlice = createSlice({
-  name: "asyncSlice",
-  initialState,
-  reducers: {
-    fetchUsersRequest(state) {
-      state.loading = true;
-    },
-    fetchUsersSuccess(state, action) {
-      state.loading = false;
-      state.users = action.payload;
-      state.error = "";
-    },
-    fetchUsersFailure(state, action) {
-      state.loading = false;
-      state.users = [];
-      state.error = action.payload;
-    },
-  },
-});
 
 // const fetchUsersThunk = createAsyncThunk("async/fetchUsers", () => {
 //   return async function (dispatch, getState) {
@@ -43,5 +24,48 @@ const asyncSlice = createSlice({
 //   };
 // });
 
-module.exports = asyncSlice.reducer;
-module.exports.asyncActions = asyncSlice.actions;
+// generates action types which have an extra property - fulfilled, pending, rejected
+const fetchUsers = createAsyncThunk("user/fetchUsers", () =>
+  axios
+    .get(`https://jsonplaceholder.typicode.com/users`)
+    .then((response) => response.data.map((user) => user.id))
+);
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    fetchUsersRequest(state) {
+      state.loading = true;
+    },
+    fetchUsersSuccess(state, action) {
+      state.loading = false;
+      state.users = action.payload;
+      state.error = "";
+    },
+    fetchUsersFailure(state, action) {
+      state.loading = false;
+      state.users = [];
+      state.error = action.payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchUsers.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      state.loading = false;
+      state.error = "";
+      state.users = action.payload;
+    });
+    builder.addCase(fetchUsers.rejected, (state, action) => {
+      state.loading = false;
+      state.users = [];
+      state.error = action.error.message;
+    });
+  },
+});
+
+module.exports = userSlice.reducer;
+module.exports.userActions = userSlice.actions;
+module.exports.fetchUsers = fetchUsers;
